f4f4499a0fb5c78abde257e88f44dd36d7cc4f9b
DELL <DELL@DESKTOP-KR4MI5N>
2026-09-24 20:42:00 +0530

    add

--- .cpp
+++ .cpp
@@ -1,10 +1,37 @@
-#include <iostream>
+# include <iostream>
 using namespace std;
+class rectangle {
 
+private:
+    float l;
+    float b;
+public:
+    void intake() {
+        cout << "enter the lenght and breadth of the rectangle" << endl;
+        cin >> l >> b;
+    }
+    float area() {
+        float ar = l * b;
+        return (ar);
+    }
+    float perimeter() {
+        float p = 2 * (l + b);
+        return (p);
+    }
+    void disply() {
+        float ar = area();
+        float p = perimeter();
+        cout << "the lenght of the rectangle is:" << l << endl;
+        cout << "the breadth of the rectangle is:" << b << endl;
+        cout << "the area of the rectangle is:" << ar << endl;
+        cout << "the perimeter of the rectangle is:" << p << endl;
+    }
+};
 int main() {
-    int a = 5;
-    int b = 10;
-    int c = a + b;
-    cout << "The sum of " << a << " and " << b << " is " << c;
+    rectangle r;
+    r.intake();
+    r.area();
+    r.perimeter();
+    r.disply();
     return 0;
 }
